5372d955e6118eea172b2a1bc3137bcebd85974f
chansol <lchans0319@gmail.com>
2025-03-16 01:43:53 +0900

    💄 영역 구분선 추가가

--- src/App.tsx
+++ src/App.tsx
@@ -22,39 +22,38 @@ export default function App() {
   const [outputs, setOutputs] = useState<OutputType>(INIT_OUTPUTS);
 
   const setValuesByKey = (key: keyof typeof INIT_INPUTS, value: string) => {
-    console.log("set values: " + value);
     setInputs((prev) => ({ ...prev, [key]: value }));
   };
 
   const handleClick = () => {
-    validateInputs(inputs);
-    const { a, b, c, face, outDial } = Object.fromEntries(
-      Object.entries(inputs).map(([key, value]) => [key, parseFloat(value)])
-    );
-    const A = calculateOutput(a, b, face, outDial);
-    const B = calculateOutput(a, c, face, outDial);
-    setOutputs({ A, B });
-  };
-
-  const validateInputs = (inputs: typeof INIT_INPUTS) => {
-    const { a, b, c } = inputs;
     try {
-      if (parseFloat(a) === 0) {
-        throw new Error("a는 0이 될 수 없습니다.");
-      }
-      if (parseFloat(a) < 0 || parseFloat(b) < 0 || parseFloat(c) < 0) {
-        throw new Error("a, b, c는 음수가 될 수 없습니다.");
-      }
-      if (parseFloat(b) >= parseFloat(c)) {
-        throw new Error("b가 c보다 작아야 합니다.");
-      }
+      validateInputs(inputs);
+      const { a, b, c, face, outDial } = Object.fromEntries(
+        Object.entries(inputs).map(([key, value]) => [key, parseFloat(value)])
+      );
+      const A = calculateOutput(a, b, face, outDial);
+      const B = calculateOutput(a, c, face, outDial);
+      setOutputs({ A, B });
     } catch (e) {
       alert(e);
     }
   };
 
+  const validateInputs = (inputs: typeof INIT_INPUTS) => {
+    const { a, b, c } = inputs;
+    if (parseFloat(a) === 0) {
+      throw new Error("a는 0이 될 수 없습니다.");
+    }
+    if (parseFloat(a) < 0 || parseFloat(b) < 0 || parseFloat(c) < 0) {
+      throw new Error("a, b, c는 음수가 될 수 없습니다.");
+    }
+    if (parseFloat(b) >= parseFloat(c)) {
+      throw new Error("b가 c보다 작아야 합니다.");
+    }
+  };
+
   return (
-    <div className="text-neutral-700 flex flex-col items-center">
+    <div className="text-neutral-700 flex flex-col items-center max-w-[840px] mx-auto">
       {/* 그래프 */}
       <div className="bg-blue-100 pl-1.5 pr-4 pt-8 pb-4 w-full">
         <div className=" w-full max-w-[800px] relative pl-[40px] mb-5 ">
@@ -125,51 +124,53 @@ export default function App() {
 
       <div className="w-full px-5 mt-8 flex flex-col gap-11">
         {/* 길이 a b c */}
-        <div className="flex flex-col gap-3">
-          <InputNumber
-            label="a"
-            labelColor="text-cyan-700"
-            value={inputs.a}
-            onChange={(value) => setValuesByKey("a", value)}
-            wrapperWidth="w-40"
-            inputWidth="w-32"
-          />
-          <InputNumber
-            label="b"
-            labelColor="text-emerald-600"
-            value={inputs.b}
-            onChange={(value) => setValuesByKey("b", value)}
-            wrapperWidth="w-40"
-            inputWidth="w-32"
-          />
-          <InputNumber
-            label="c"
-            labelColor="text-purple-800"
-            value={inputs.c}
-            onChange={(value) => setValuesByKey("c", value)}
-            wrapperWidth="w-40"
-            inputWidth="w-32"
-          />
-        </div>
+        <div className="flex flex-wrap gap-11">
+          <div className="flex flex-col gap-3">
+            <InputNumber
+              label="a"
+              labelColor="text-cyan-700"
+              value={inputs.a}
+              onChange={(value) => setValuesByKey("a", value)}
+              wrapperWidth="w-40"
+              inputWidth="w-32"
+            />
+            <InputNumber
+              label="b"
+              labelColor="text-emerald-600"
+              value={inputs.b}
+              onChange={(value) => setValuesByKey("b", value)}
+              wrapperWidth="w-40"
+              inputWidth="w-32"
+            />
+            <InputNumber
+              label="c"
+              labelColor="text-purple-800"
+              value={inputs.c}
+              onChange={(value) => setValuesByKey("c", value)}
+              wrapperWidth="w-40"
+              inputWidth="w-32"
+            />
+          </div>
 
-        {/* 기타 입력값 face OD*/}
-        <div className="flex flex-col gap-3">
-          <InputNumber
-            label="Face"
-            value={inputs.face}
-            onChange={(value) => setValuesByKey("face", value)}
-            wrapperWidth="w-[300px]"
-            inputWidth="w-44"
-          />
-          <InputNumber
-            label="Out Dial"
-            value={inputs.outDial}
-            onChange={(value) => setValuesByKey("outDial", value)}
-            wrapperWidth="w-[300px]"
-            inputWidth="w-44"
-          />
+          {/* 기타 입력값 face OD*/}
+          <div className="flex flex-col gap-3">
+            <InputNumber
+              label="Face"
+              value={inputs.face}
+              onChange={(value) => setValuesByKey("face", value)}
+              wrapperWidth="w-72"
+              inputWidth="w-44"
+            />
+            <InputNumber
+              label="Out Dial"
+              value={inputs.outDial}
+              onChange={(value) => setValuesByKey("outDial", value)}
+              wrapperWidth="w-72"
+              inputWidth="w-44"
+            />
+          </div>
         </div>
-
+        <div className="border border-neutral-300" />
         {/* 결과 A B */}
         <div className="flex flex-col gap-3">
           <Text title="A" value={outputs.A?.toString() || "-"} />
@@ -178,7 +179,7 @@ export default function App() {
       </div>
 
       {/* 계산 버튼 */}
-      <div className="max-w-[400px]">
+      <div className="max-w-[600px] w-full mt-11 flex">
         <Button text="계산하기" onClick={handleClick} />
       </div>
       <Footnote />
@@ -188,10 +189,10 @@ export default function App() {
 
 function Footnote() {
   return (
-    <div className="flex flex-col gap-1.5 mt-12 w-full px-12 relative">
-      <span className="text-red-700 text-2xl absolute top-[-16px] left-[28px]">
-        *
-      </span>
+    <div className="flex flex-col gap-1.5 mt-12 w-full px-5 relative mb-14">
+      <h4 className="text-red-700 border-b border-neutral-400 pb-1.5 font-semibold">
+        축 정렬 공식
+      </h4>
       <span>A = b/a * F + O.D / 2</span>
       <span>B = c/a * F + O.D / 2</span>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ text, onClick }: Props) {
   return (
     <button
       onClick={onClick}
-      className="w-full border-2 border-neutral-300 hover:bg-neutral-200 active:bg-neutral-300 cursor-pointer text-3xl rounded-sm py-5 px-16"
+      className="grow border-2 border-neutral-300 hover:bg-neutral-200 active:bg-neutral-300 cursor-pointer text-3xl rounded-sm mx-4 py-5 px-16"
     >
       {text}
     </button>
